3c73ab79732c4e47acc5cec4e1a0b8f920c33a0d
Andrey Arakelyants <arakelyants.andrey@yandex.ru>
2024-03-02 18:41:38 +0200

    receive first messages

--- telemetry-adapter/app/infrastructure/clients/sqs.py
+++ telemetry-adapter/app/infrastructure/clients/sqs.py
@@ -1,5 +1,7 @@
 import logging
 
+import boto3
+
 from app.infrastructure.clients.interfaces import QueueClient
 
 
@@ -8,10 +10,16 @@ logger = logging.getLogger(__file__)
 
 class SQSClient(QueueClient):
     def __init__(self, queue_url):
+        self.sqs_client = boto3.client("sqs", endpoint_url="http://localstack:4566")
         self.queue_url = queue_url
 
     def get_messages(self):
-        logger.debug("get messages")
+        logger.debug(f"get messages from {self.queue_url}")
+        messages = self.sqs_client.receive_message(
+            QueueUrl=self.queue_url,
+            AttributeNames=['All']
+        )
+        return messages
 
     def delete_message(self, id_):
         logger.debug("delete messages")
